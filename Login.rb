95f2b60fe0d537cb45fc05879eab16e7c7b14cf8
AtanimoGames <46455759+AtanimoGames@users.noreply.github.com>
2020-01-30 14:32:08 +0200

    Update Login.rb

--- Login.rb
+++ Login.rb
@@ -1,11 +1,26 @@
+system "clear"
+
+signup = gets.chomp
+login = gets.chomp
+
+puts "Type: Login to login to exsist account\nOr type: Signup to register."
+
+if signup == "signup" or signup == "Signup" or signup == "SIGNUP"
+
+
+
 puts "Register a new user\n ___________"
 
 puts "Enter your username"
+transaction = Array.new
 regname = gets.chomp
+transaction.insert(regname)
 system "clear"
 
 puts "Enter your password"
+transaction = Array.new
 regpass = gets.chomp
+transaction.insert(regpass)
 system "clear"
 
 puts "Reading DataBase..."
@@ -21,10 +36,13 @@ puts "Please enter your password."
 
 password = gets.chomp
 
+end 
 
+if login == "login" or login == "Login" or login == "LOGIN"
 
 if password == regpass and username == regname
   puts "Welcome #{username}.\n "
   else
   puts "Invalid password!"
+	end
 end
